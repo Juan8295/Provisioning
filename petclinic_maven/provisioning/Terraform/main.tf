d5d5dd1ba43457131a74f92105c7220eac30db9e
Juan Goez <juan.goez.cordoba@devoteam.com>
2022-04-05 19:28:16 +0200

    tets

--- petclinic_maven/provisioning/Terraform/main.tf
+++ petclinic_maven/provisioning/Terraform/main.tf
@@ -29,7 +29,7 @@ resource "azurerm_virtual_network" "vnet" {
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
   resource_group_name  = var.resource_group_name
-  virtual_network_name = var.virtual_network_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
